2896ab08ed9d436a3b149ae4515033b02c8c537d
unknown <pfgallardo1987@gmail.com>
2021-03-23 22:00:13 -0300

    commit envio de correo

--- ProyectoWeb/contacto/views.py
+++ ProyectoWeb/contacto/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 
 from .forms import FormularioContacto
 
+from django.core.mail import EmailMessage
+
 # Create your views here.
 
 def contacto(request):
@@ -14,6 +16,17 @@ def contacto(request):
             email=request.POST.get("email")
             contenido=request.POST.get("contenido")
 
-            return redirect("/contacto/?valido")
+            email=EmailMessage("Mensaje desde app django",
+            "El usuario con nombre {} con la direccion {} escribe lo siguiente: {}".format(nombre,email,contenido),
+            "",["pfgallardo1987@gmail.com"],reply_to=[email])
+
+            try:
+                email.send()
+
+
+                return redirect("/contacto/?valido")
+            except:
+                return redirect("/contacto/?novalido")
+
 
     return render(request, "contacto/contacto.html", {'miFormulario':formulario_contacto})
